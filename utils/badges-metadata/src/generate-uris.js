962af24208a6c72ce63852f555b50b3876a52377
anotherdev <joseluismanco37@gmail.com>
2025-10-28 19:45:36 -0500

    feat: add self-verification badge

--- utils/badges-metadata/src/generate-uris.js
+++ utils/badges-metadata/src/generate-uris.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const BADGES_FILE_PATH = './badges.json';
 const OUTPUT_FILE = './badges-with-uris.json';
-const IPFS_BASE_URI = 'ipfs/QmYSzCMrr8GWH29t6jPH5njB9YSd4tb8XLEqha8ynZgrPz';
+const IPFS_BASE_URI = 'ipfs/QmV788xYJFJ5ZTXKhE3ATNV3XhbvDg9k5SEbVNnnFdKvKe';
 
 function generateFileName(badgeId, levelId) {
   const paddedBadgeId = badgeId.toString().padStart(64, '0');
